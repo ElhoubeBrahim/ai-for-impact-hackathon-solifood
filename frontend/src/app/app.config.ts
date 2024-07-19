c30d811dae132fbcf94050f2994618fc1a0e3b69
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-07-19 21:10:32 +0100

    frontend: firebase providers setup

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (environment.useEmulators) {
+        connectAuthEmulator(auth, 'http://localhost:9099');
+      }
+      return auth;
+    }),
+    provideToastr(),
+  ],
 };
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  useEmulators: true,
+  apiURL: 'https://localhost:8000/api',
+  searchAPI: 'https://search-api.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyB3K6JCnio9Kx8ZX4XpVzTemS3_iubqb1E',
+    authDomain: 'ai-for-impact-solifood-ca41a.firebaseapp.com',
+    projectId: 'ai-for-impact-solifood-ca41a',
+    storageBucket: 'ai-for-impact-solifood-ca41a.appspot.com',
+    messagingSenderId: '401213418462',
+    appId: '1:401213418462:web:16323661d1421cac3c0b4f',
+  },
+};
