4dbc37dbe0814a0faa5360630fe8c3cbc9e26849
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-07-19 20:51:52 +0100

    frontend: define main entities types

---
+++ frontend/src/app/core/models/basket.ts
@@ -0,0 +1,21 @@
+import { Timestamp } from '@angular/fire/firestore';
+import { User } from './user';
+
+export interface Basket {
+  id: string;
+  title: string;
+  description: string;
+  images: string[];
+  realPrice: number;
+  price: number;
+  location: { lat: number; lon: number };
+  available: boolean;
+  blocked: boolean;
+  tags: string[];
+  ingredients: string[];
+  createdBy: User;
+  claimedBy: User | null;
+  expiredAt: Timestamp;
+  soldAt: Timestamp | null;
+  createdAt: Timestamp;
+}
