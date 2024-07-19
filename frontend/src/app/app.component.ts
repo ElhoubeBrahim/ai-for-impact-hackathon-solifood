9940c467f0ff81220eb0ef3e80c20ca12210cc83
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-07-19 22:57:51 +0100

    frontend: get current loggedin user

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { Auth } from '@angular/fire/auth';
+import { StorageService } from './core/services/storage.service';
+import { AuthenticationService } from './core/services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +15,21 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent implements OnInit {
   private router = inject(Router);
+  private auth = inject(Auth);
+  private storage = inject(StorageService);
+  private authentication = inject(AuthenticationService);
 
   ngOnInit() {
+    // Load user data
+    this.auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const result = await this.authentication.getCurrentUser();
+        this.storage.user = result.user || undefined;
+      }
+
+      this.storage.user = undefined;
+    });
+
     // Scroll to top on route change
     this.router.events.subscribe((event: any) => {
       if (event instanceof NavigationEnd) {
