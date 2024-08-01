3b0359c370a98dd2713cbc1fe91eb96273607fba
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2024-08-01 02:49:24 +0100

    dashboard : create layout admin

---
+++ dashboard/src/app/layout/admin/admin.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/component/header/header.component';
+import { FooterComponent } from '../../shared/component/footer/footer.component';
+import { LoadingComponent } from '../../shared/page/loading/loading.component';
+import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from "../../shared/component/side-bar/side-bar.component";
+import { NavBarComponent } from "../../shared/component/nav-bar/nav-bar.component";
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [LoadingComponent, RouterOutlet, SideBarComponent, NavBarComponent, FooterComponent],
+  templateUrl: './admin.component.html'
+})
+export class AdminComponent {
+
+}
