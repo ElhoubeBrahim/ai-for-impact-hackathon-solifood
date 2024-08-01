8a32726d169e4fcf4c27b4ca6600dee6094a0b5c
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2024-08-01 17:37:09 +0100

    dashboard: implement routing

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './layout/admin/admin.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:AdminComponent, children:[
+        {path:'',component:DashboardComponent},
+    ]}
+];
