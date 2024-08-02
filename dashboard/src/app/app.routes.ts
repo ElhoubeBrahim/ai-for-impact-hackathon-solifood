bee428be1d4bfb04ca852ffcbc36ed53ceff38a9
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2024-08-02 01:31:22 +0100

    dashboard : add routing basket

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './layout/admin/admin.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { BasketComponent } from './features/basket/basket.component';
+
 
 export const routes: Routes = [
     {path: '', component:AdminComponent, children:[
         {path:'',component:DashboardComponent},
+        {path:'basket',component:BasketComponent},
+
     ]}
 ];
