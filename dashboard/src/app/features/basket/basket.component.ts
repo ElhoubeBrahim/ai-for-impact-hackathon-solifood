f2db88dd4a2deddac104cba9676f0df789954b51
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2024-08-02 01:29:54 +0100

    dashboard: implement basket page

---
+++ dashboard/src/app/features/basket/basket.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { Basket } from '../../core/model/basket';
+import { ItemBasketComponent } from './item-basket/item-basket.component';
+import { LoadingComponent } from '../../shared/page/loading/loading.component';
+import { NoDataComponent } from '../../shared/component/no-data/no-data.component';
+import { HttpService } from '../../core/services/http.service';
+
+@Component({
+  selector: 'app-basket',
+  standalone: true,
+  imports: [ItemBasketComponent,LoadingComponent,NoDataComponent],
+  templateUrl: './basket.component.html'
+})
+export class BasketComponent implements OnInit{
+  @Input() isSearchMode = false;
+  http = inject(HttpService)
+  baskets: Basket[] = [];
+  endReached = false;
+  basketsLoading = false;
+  loading = false;
+
+  ngOnInit(): void {
+    this.http.get<Basket[]>("/data/baskets.json").subscribe(
+      (data:Basket[])=>{
+        this.baskets = data;
+      }
+    )
+  }
+}
