ec649c517986c151b52928333b66e33f1ba5e009
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-08-03 12:13:53 +0100

    frontend: calculate actual distance from the user location

--- frontend/src/app/shared/basket/basket.component.ts
+++ frontend/src/app/shared/basket/basket.component.ts
@@ -1,39 +1,62 @@
-import { Component, Input } from "@angular/core";
-import { Basket } from "../../core/models/basket";
-import { RouterLink } from "@angular/router";
-import dayjs from "dayjs";
+import { Component, Input } from '@angular/core';
+import { Basket } from '../../core/models/basket';
+import { RouterLink } from '@angular/router';
+import dayjs from 'dayjs';
 
-import relativeTime from "dayjs/plugin/relativeTime";
+import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
 @Component({
-  selector: "app-basket",
+  selector: 'app-basket',
   standalone: true,
   imports: [RouterLink],
-  templateUrl: "./basket.component.html",
+  templateUrl: './basket.component.html',
 })
 export class BasketComponent {
   @Input() basket: Basket | null = null;
-
-  // willExpireSoon(): boolean {
-  //   if (!this.basket) return false;
-
-  //   const now = dayjs();
-  //   const willExpireAt = dayjs(this.basket.expiredAt.toDate());
-
-  //   return willExpireAt.diff(now, "day") < 1; // less than 1 day
-  // }
-
-  // expired(): boolean {
-  //   if (!this.basket) return false;
-
-  //   const now = dayjs();
-  //   const expiredAt = dayjs(this.basket.expiredAt.toDate());
-
-  //   return expiredAt.isBefore(now);
-  // }
+  @Input() userLocation: { lat: number; lon: number } = { lat: 0, lon: 0 };
 
   timeAgo(date: Date): string {
     return dayjs(date).fromNow();
+  }
+
+  calculateDistance() {
+    const location = this.basket?.location;
+    const center = this.userLocation;
+
+    if (!location) {
+      return 0;
+    }
+
+    const R = 6371; // Earth's radius in kilometers
+    const dLat = this.toRadians(center.lat - location.lat);
+    const dLon = this.toRadians(center.lon - location.lon);
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.toRadians(location.lat)) *
+        Math.cos(this.toRadians(center.lat)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+
+    return this.formatDistance(Number(distance.toFixed(2)));
+  }
+
+  private toRadians(degrees: number): number {
+    return degrees * (Math.PI / 180);
+  }
+
+  private formatDistance(distance: number): string {
+    if (distance < 1) {
+      const meters = Math.round(distance * 1000);
+      return `${meters} ${meters === 1 ? 'meter' : 'meters'} away`;
+    } else if (distance < 10) {
+      return `${distance.toFixed(1)} km away`;
+    } else {
+      return `${Math.round(distance)} km away`;
+    }
   }
 }
