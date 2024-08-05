c29463e6720dd1dd6ad8fe4d09e480547ef86644
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-08-05 15:57:18 +0100

    mobile : add tailwind

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Google Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
