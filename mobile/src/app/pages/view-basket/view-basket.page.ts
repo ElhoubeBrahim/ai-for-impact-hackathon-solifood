199d7ac036ccdfcc84b29dffececeffef53f893f
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-08-06 16:12:33 +0100

    mobile : add view basket page

--- mobile/src/app/pages/view-basket/view-basket.page.ts
+++ mobile/src/app/pages/view-basket/view-basket.page.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Platform, IonHeader, IonToolbar, IonButtons, IonBackButton, IonContent, IonItem, IonIcon, IonLabel, IonNote} from '@ionic/angular/standalone';
+import { Platform, IonHeader, IonToolbar, IonButtons, IonBackButton, IonContent, IonItem, IonIcon, IonLabel, IonNote, IonTitle, IonButton } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { personCircle } from 'ionicons/icons';
 import { Timestamp } from '@angular/fire/firestore';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
 @Component({
   selector: 'app-view-basket',
   templateUrl: './view-basket.page.html',
   standalone: true,
-  imports: [ IonHeader, IonToolbar, IonButtons, IonBackButton, IonContent, IonItem, IonIcon, IonLabel, IonNote, CommonModule]
+  imports: [IonHeader, IonToolbar, IonButtons,IonButton, IonBackButton, IonContent, IonItem, IonIcon, IonLabel, IonNote, IonTitle, ButtonComponent, CommonModule]
 })
-export class ViewBasketPage {
+export class ViewBasketPage implements OnInit {
   public basket = {
     id: "1",
     title: "Basket 1",
@@ -20,6 +21,8 @@ export class ViewBasketPage {
     images: [
       "https://images.radio-canada.ca/v1/alimentation/recette/16x9/ogleman-spaghetti-boulettes.jpg",
       "https://images.radio-canada.ca/v1/alimentation/recette/16x9/ogleman-spaghetti-boulettes.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSokLLpaQWJPa-IJ33ei_rWnxY7Z1Qi5GLZhw&s"
+
     ],
     location: { lat: 123.456, lon: 789.012 },
     available: true,
@@ -43,9 +46,11 @@ export class ViewBasketPage {
     claimedBy: null,
     soldAt: null
   };
-
+  currentImage: string = "";
+  currentIndex: number = 0;
   private activatedRoute = inject(ActivatedRoute);
   private platform = inject(Platform);
+  activeImage = 0;
 
   constructor() {
     addIcons({ personCircle });
@@ -55,6 +60,18 @@ export class ViewBasketPage {
   //   // const id = this.activatedRoute.snapshot.paramMap.get('id') as string;
   //   // this.message = this.data.getMessageById(parseInt(id, 10));
   // }
+
+  ngOnInit() {
+    this.startImageRotation();
+  }
+
+  startImageRotation() {
+    this.currentImage = this.basket.images[this.currentIndex];
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.basket.images.length;
+      this.currentImage = this.basket.images[this.currentIndex];
+    }, 3000);
+  }
 
   getBackButtonText() {
     const isIos = this.platform.is('ios')
