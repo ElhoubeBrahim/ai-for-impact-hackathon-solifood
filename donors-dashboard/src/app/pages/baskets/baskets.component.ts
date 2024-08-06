4fa94888665b37af3ac66bfd1fb17e243c307647
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-08-06 23:01:35 +0100

    dashboard: list donor(s baskets

--- donors-dashboard/src/app/pages/baskets/baskets.component.ts
+++ donors-dashboard/src/app/pages/baskets/baskets.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { Basket } from '../../core/model/basket';
+import { HttpService } from '../../core/services/http.service';
+import { LoadingComponent } from '../../shared/loading/loading.component';
+import { NoDataComponent } from '../../shared/component/no-data/no-data.component';
+import { ItemBasketComponent } from './item-basket/item-basket.component';
+import { AuthenticationService } from '../../core/services/authentication.service';
+import { BasketService } from '../../core/services/basket.service';
 
 @Component({
   selector: 'app-basket',
   standalone: true,
-  imports: [],
+  imports: [ItemBasketComponent, LoadingComponent, NoDataComponent],
   templateUrl: './baskets.component.html',
 })
-export class BasketsComponent {}
+export class BasketsComponent implements OnInit {
+  @Input() isSearchMode = false;
+  http = inject(HttpService);
+  authentication = inject(AuthenticationService);
+  basketService = inject(BasketService);
+
+  baskets: Basket[] = [];
+  endReached = false;
+  basketsLoading = false;
+  loading = true;
+
+  ngOnInit(): void {
+    this.getBaskets();
+  }
+
+  async getBaskets() {
+    const currentUser = await this.authentication.getCurrentUser();
+    if (!currentUser.user) {
+      console.error('User not found:', currentUser.error);
+      this.loading = false;
+      return;
+    }
+
+    this.basketService
+      .getBasketsByUser(currentUser.user.id)
+      .subscribe((baskets) => {
+        this.baskets = baskets;
+        this.loading = false;
+      });
+  }
+}
---
+++ donors-dashboard/src/app/pages/baskets/item-basket/item-basket.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { Basket } from '../../../core/model/basket';
+import dayjs from 'dayjs';
+
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { RouterLink } from '@angular/router';
+dayjs.extend(relativeTime);
+
+@Component({
+  selector: 'app-item-basket',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './item-basket.component.html',
+})
+export class ItemBasketComponent {
+  @Input() basket: Basket | null = null;
+
+  timeAgo(date: Date): string {
+    return dayjs(date).fromNow();
+  }
+}
