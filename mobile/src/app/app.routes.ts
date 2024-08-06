9d2305bd22e1199dd7c910aa7a19052e3c19a89a
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-08-07 00:19:26 +0100

    mobile : add firebase guard

--- mobile/src/app/app.routes.ts
+++ mobile/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
+
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
   },
   {
     path: '',
@@ -12,27 +19,23 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage),
+    ...canActivate(() => redirectLoggedInTo(['/explore'])),
   },
   {
     path: 'signup',
-    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
+    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage),
+    ...canActivate(() => redirectLoggedInTo(['/explore'])),
   },
   {
     path: 'explore',
-    loadComponent: () => import('./pages/explore/explore.page').then( m => m.ExplorePage)
-  },
-  {
-    path: 'reset-password',
-    loadComponent: () => import('./pages/reset-password/reset-password.page').then( m => m.ResetPasswordPage)
-  },
-  {
-    path: 'basket',
-    loadComponent: () => import('./pages/basket/basket.page').then( m => m.BasketPage)
+    loadComponent: () => import('./pages/explore/explore.page').then( m => m.ExplorePage),
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
   },
    {
     path: 'basket/:id',
-    loadComponent: () => import('./pages/view-basket/view-basket.page').then( m => m.ViewBasketPage)
+    loadComponent: () => import('./pages/view-basket/view-basket.page').then( m => m.ViewBasketPage),
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
 
   },
 ];
