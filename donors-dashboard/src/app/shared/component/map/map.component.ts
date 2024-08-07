6f015ca8a9c442b46cab6a446960b0f3e77b8cda
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-08-07 14:48:31 +0100

    dashboard: fix map component location update issue

--- donors-dashboard/src/app/shared/component/map/map.component.ts
+++ donors-dashboard/src/app/shared/component/map/map.component.ts
@@ -1,16 +1,32 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { LeafletModule } from "@asymmetrik/ngx-leaflet";
-import { Layer, MapOptions, icon, latLng, marker, tileLayer } from "leaflet";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import {
+  Layer,
+  Map,
+  MapOptions,
+  icon,
+  latLng,
+  marker,
+  tileLayer,
+} from 'leaflet';
 
 type Location = { latitude: number; longitude: number };
 
 @Component({
-  selector: "app-map",
+  selector: 'app-map',
   standalone: true,
   imports: [LeafletModule],
-  templateUrl: "./map.component.html",
+  templateUrl: './map.component.html',
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnChanges {
   @Output() locationChange = new EventEmitter<Location>();
 
   @Input() location: Location | null = null;
@@ -20,21 +36,42 @@ export class MapComponent implements OnInit {
 
   options: MapOptions = {
     layers: [
-      tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
       }),
     ],
     zoom: this.zoom,
+    center: latLng(this.center.latitude, this.center.longitude),
   };
 
   layers: Layer[] = [];
+  map: Map | null = null;
 
   ngOnInit(): void {
+    this.updateMap();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['location'] || changes['center'] || changes['zoom']) {
+      this.updateMap();
+    }
+  }
+
+  updateMap(): void {
     if (this.location) {
       this.layers = [this.createMarker(this.location)];
     }
 
-    this.options.center = latLng(this.center.latitude, this.center.longitude);
+    if (this.map) {
+      this.map.setView(
+        latLng(this.center.latitude, this.center.longitude),
+        this.zoom
+      );
+    }
+  }
+
+  onMapReady(map: Map): void {
+    this.map = map;
   }
 
   onMapClick(event: any): void {
@@ -48,7 +85,7 @@ export class MapComponent implements OnInit {
   createMarker(location: Location): Layer {
     return marker([location.latitude, location.longitude], {
       icon: icon({
-        iconUrl: "/assets/marker-icon.png",
+        iconUrl: '/assets/marker-icon.png',
         iconAnchor: [19, 35],
       }),
     });
