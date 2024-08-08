9cbe96832af03b602112424190f544474d6b57a3
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-08-08 15:58:27 +0100

    dashboard: infinite scroll orders pagination

---
+++ donors-dashboard/src/app/core/services/order.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order, OrderStatus } from '../model/order';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  constructor(private http: HttpClient) {}
+
+  getOrders(
+    params: {
+      lastDocId?: string;
+      status?: OrderStatus;
+    } = {}
+  ): Observable<Order[]> {
+    const { lastDocId, status } = params;
+
+    let httpParams = new HttpParams();
+
+    if (lastDocId) {
+      httpParams = httpParams.set('lastDocId', lastDocId);
+    }
+
+    if (status) {
+      httpParams = httpParams.set('status', status);
+    }
+
+    return this.http.get<Order[]>('/orders', { params: httpParams });
+  }
+}
--- donors-dashboard/src/app/pages/orders/orders.component.ts
+++ donors-dashboard/src/app/pages/orders/orders.component.ts
@@ -3,40 +3,60 @@ import { NoDataComponent } from '../../shared/component/no-data/no-data.componen
 import { CommonModule } from '@angular/common';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
 import { MatDialog } from '@angular/material/dialog';
-import { ItemBasketComponent } from '../baskets/item-basket/item-basket.component';
 import { LoadingComponent } from '../../shared/loading/loading.component';
 import { HttpService } from '../../core/services/http.service';
 import { Order } from '../../core/model/order';
 import { RouterLink } from '@angular/router';
 import dayjs from 'dayjs';
+import { OrderService } from '../../core/services/order.service';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-order',
   standalone: true,
   imports: [
-    ItemBasketComponent,
     LoadingComponent,
     NoDataComponent,
     CommonModule,
     RouterLink,
+    InfiniteScrollDirective,
   ],
   templateUrl: './orders.component.html',
 })
 export class OrdersComponent implements OnInit {
-  @Input() isSearchMode = false;
   http = inject(HttpService);
+  orderService = inject(OrderService);
+
   orders: Order[] = [];
   endReached = false;
-  basketsLoading = false;
-  loading = false;
+  ordersLoading = false;
+  loading = true;
 
   ngOnInit(): void {
-    this.http.get<Order[]>('orders').subscribe((orders) => {
+    this.orderService.getOrders().subscribe((orders) => {
       this.orders = orders;
+      this.loading = false;
     });
   }
 
   constructor(public dialog: MatDialog) {}
+
+  async handleScroll() {
+    if (this.ordersLoading || this.endReached) return;
+    this.ordersLoading = true;
+
+    const lastResult = this.orders[this.orders.length - 1];
+    this.orderService
+      .getOrders({ lastDocId: lastResult.id })
+      .subscribe((orders) => {
+        if (orders.length === 0) {
+          this.endReached = true;
+        } else {
+          this.orders.push(...orders);
+        }
+        this.ordersLoading = false;
+      });
+  }
 
   openDialog(data: Order) {
     this.dialog.open(OrderDetailComponent, {
